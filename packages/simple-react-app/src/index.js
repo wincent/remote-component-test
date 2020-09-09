843c822ac0a26414c92cf77d6bfb14cfc9d7b7bf
Greg Hurrell <greg.hurrell@liferay.com>
2020-09-09 18:50:18 +0200

    feat: add some interactivity to the React example

--- packages/simple-react-app/src/index.js
+++ packages/simple-react-app/src/index.js
@@ -1,10 +1,28 @@
-import React from 'react';
+import React, {useState} from 'react';
 import ReactDOM from 'react-dom';
 
 class App extends React.Component {
 	render() {
-		return <h1>Hi</h1>;
+		return <Greeter />;
 	}
+}
+
+function Greeter() {
+	const [name, setName] = useState('Jane Tester');
+
+	return (
+		<div>
+			<label>
+				<span>Name:</span>
+				<input
+					onChange={(event) => setName(event.target.value)}
+					type="text"
+					value={name}
+				/>
+			</label>
+			<p>Hello, {name}!</p>
+		</div>
+	);
 }
 
 ReactDOM.render(<App />, document.getElementById('root'));
