32879813bc9b50175390d8e0af85f5be916d7b66
Greg Hurrell <greg.hurrell@liferay.com>
2020-09-09 13:27:30 +0200

    feat: add seed for initial gh-pages
    Just so that I can see it working.

---
+++ index.js
@@ -0,0 +1,6 @@
+(function () {
+	'use strict';
+
+	// Simple demo to show that we can load and run JS from gh-pages.
+	document.getElementById('status').innerText = 'all systems operational';
+})();
