e4fc959325cf3741145d2ce6eab64007a888caeb
Greg Hurrell <greg.hurrell@liferay.com>
2020-09-09 19:41:36 +0200

    chore: commit simple-react-app build artifacts (autogenerated)

--- packages/simple-react-app/dist/index.js
+++ packages/simple-react-app/dist/index.js
@@ -28386,7 +28386,11 @@
 
 	  }
 
-	  customElements.define('simple-react-app', SimpleReactApp);
+	  if (customElements.get('simple-react-app')) {
+	    console.log('Skipping registration for <simple-react-app> (already registered)');
+	  } else {
+	    customElements.define('simple-react-app', SimpleReactApp);
+	  }
 	}
 
 }());
