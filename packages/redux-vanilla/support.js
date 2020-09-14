7fd67e536e541d9b5039e343d2409207a29a6a3b
Greg Hurrell <greg.hurrell@liferay.com>
2020-09-14 16:41:32 +0200

    style: run `yarn format`

--- packages/redux-vanilla/support.js
+++ packages/redux-vanilla/support.js
@@ -9,7 +9,7 @@
 	let subscriber;
 	let text = 'Lorem ipsum';
 
-	const WORDS = (`
+	const WORDS = `
 		Lorem ipsum dolor sit amet consectetur adipiscing elit sed do
 		eiusmod tempor incididunt ut labore et dolore magna aliqua Ut
 		enim ad minim veniam quis nostrud exercitation ullamco laboris
@@ -17,7 +17,9 @@
 		reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
 		pariatur Excepteur sint occaecat cupidatat non proident sunt in
 		culpa qui officia deserunt mollit anim id est laborum
-	`).trim().split(/\s+/);
+	`
+		.trim()
+		.split(/\s+/);
 
 	function getRandomWord() {
 		return WORDS[Math.floor(Math.random() * WORDS.length)];
