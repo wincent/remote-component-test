69b86c67d6de228b0b342a01475edf4fd6f09868
Greg Hurrell <greg.hurrell@liferay.com>
2020-09-16 18:37:31 +0200

    refactor: turn off a standalone-only demo feature
    For consistency with what you'll see in DXP. Disabling the code instead
    of deleting it because it still shows a potential useful capability.

--- packages/simple-react-app/src/index.js
+++ packages/simple-react-app/src/index.js
@@ -132,7 +132,20 @@ if (customElements.get('simple-react-app')) {
 
 const container = document.getElementById('simple-react-app-standalone-root');
 
-if (container) {
+// Not really relevant to the initial proof-of-concept in DXP, but you can set
+// this to `true` in the standalone demo to show bidirectional flow between the
+// React web app inside the web component and the outside world.
+//
+// This adds a button that shows that you can:
+//
+// - `setAttribute` on the web component and have that data show up in the React
+//   app; and:
+// - `setAttribute` on the web component to register a callback, which the React
+//   app can the use to communicate information back to the outside world.
+
+const demoBidirectionalDataFlow = false;
+
+if (container && demoBidirectionalDataFlow) {
 	// We're probably being rendered at:
 	//
 	// http://remote-component-test.wincent.com/packages/simple-react-app/index.html
