f96e01b0ca060ef1dbf6578afc0ffd1ee036c3b2
Raymond Augé <raymond.auge@liferay.com>
2020-09-16 12:11:58 -0400

    feat: show component details on top-level index
    Signed-off-by: Raymond Augé <raymond.auge@liferay.com>

--- index.js
+++ index.js
@@ -3,4 +3,7 @@
 
 	// Simple demo to show that we can load and run JS from gh-pages.
 	document.getElementById('status').innerText = 'all systems operational';
+	document.querySelectorAll("span.ADDRESS").forEach((span) => {
+		span.innerText = location.protocol + '//' + location.host;
+	});
 })();
